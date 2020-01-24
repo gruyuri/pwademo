2d7470b90002fdff05afbd23f024bf6b13301298
yuri.grushovskiy <Yuri.Grushovskiy@pin-ag.de>
2020-01-24 11:51:28 +0100

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item, ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwademo';
+  items: Array<Item>;
+
+  constructor(private svcApi : ApiService) {}
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.svcApi.fetch().subscribe(
+      (data: Array<Item>) => {
+        console.log(data);
+        this.items = data;
+      }, (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
